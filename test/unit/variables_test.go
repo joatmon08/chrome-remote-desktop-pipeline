76b19ab7cc88622e9c4b4c7c097b7fafcb03da29
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2019-10-20 18:15:08 -0400

    Add unit test to check Google application creds

--- test/unit/variables_test.go
+++ test/unit/variables_test.go
@@ -19,3 +19,17 @@ func TestTFCloudCredentials(t *testing.T) {
 		t.Errorf("%s does not have content", tfCloudFile)
 	}
 }
+
+func TestGoogleCredentials(t *testing.T) {
+	filename, ok := os.LookupEnv("GOOGLE_APPLICATION_CREDENTIALS")
+	if !ok {
+		t.Errorf("%s is undefined", "GOOGLE_APPLICATION_CREDENTIALS")
+	}
+	info, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		t.Errorf("%s does not exist", filename)
+	}
+	if info.Size() == 0 {
+		t.Errorf("%s does not have content", filename)
+	}
+}
