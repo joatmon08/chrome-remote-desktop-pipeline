2ff82ee9169e3854cf6bb69dbdbe8cc3195dbdb8
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2019-10-21 13:17:13 -0400

    Update the crd script

--- scripts/crd.sh
+++ scripts/crd.sh
@@ -5,5 +5,9 @@ USER=$(terraform output user)
 CODE=$(terraform output crd_code)
 PIN=$(terraform output crd_pin)
 
+echo $SSH_PUBLIC_KEY > ${HOME}/.ssh/google_compute_engine.pub
+chmod 0600 ${HOME}/.ssh/google_compute_engine.pub
+cp ${HOME}/.ssh/id_rsa_aede9c90744f2d2ba8aa78236a99a10b ${HOME}/.ssh/google_compute_engine
+
 gcloud auth activate-service-account packer-builder@hc-da-test.iam.gserviceaccount.com --key-file=${GOOGLE_APPLICATION_CREDENTIALS}
 gcloud compute ssh --zone ${GCLOUD_ZONE} ${USER}@${HOSTNAME} --command 'DISPLAY= /opt/google/chrome-remote-desktop/start-host --code=\"${CODE}\" --redirect-url=\"https://remotedesktop.google.com/_/oauthredirect\" --name=${HOSTNAME} --pin=${PIN}'
