c0ba7aaabec0eb11e7da5d38fd9b6d0e9eea4139
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2019-10-21 20:47:20 -0400

    Add script

--- scripts/crd.sh
+++ scripts/crd.sh
@@ -11,4 +11,4 @@ chmod 0600 ${HOME}/.ssh/google_compute_engine.pub
 cp ${HOME}/.ssh/id_rsa_aede9c90744f2d2ba8aa78236a99a10b ${HOME}/.ssh/google_compute_engine
 gcloud config set project ${GCLOUD_PROJECT}
 gcloud auth activate-service-account packer-builder@hc-da-test.iam.gserviceaccount.com --key-file=${GOOGLE_APPLICATION_CREDENTIALS}
-gcloud compute ssh --zone ${GCLOUD_ZONE} ${USER}@${HOSTNAME} --command 'DISPLAY= /opt/google/chrome-remote-desktop/start-host --name=\"${HOSTNAME}\" --pin=\"${PIN}\" --code=\"${CODE}\" --redirect-url=\"https://remotedesktop.google.com/_/oauthredirect\"'
+gcloud compute ssh --zone ${GCLOUD_ZONE} ${USER}@${HOSTNAME} --command "DISPLAY= /opt/google/chrome-remote-desktop/start-host --name=\"${HOSTNAME}\" --pin=\"${PIN}\" --code=\"${CODE}\" --redirect-url=\"https://remotedesktop.google.com/_/oauthredirect\""
