90ac693b2ef55d075c9f8a0054a8fbdbc51d1574
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2019-10-27 21:24:31 -0400

    Parametrize remote desktop initialization

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -10,5 +10,5 @@ GCLOUD_PROJECT=$(terraform output project)
 GCLOUD_ZONE=$(terraform output gcloud_zone)
 
 gcloud config set project ${GCLOUD_PROJECT}
-gcloud auth activate-service-account packer-builder@hc-da-test.iam.gserviceaccount.com --key-file=${GOOGLE_APPLICATION_CREDENTIALS}
+gcloud auth activate-service-account packer-builder@${GCLOUD_PROJECT}.iam.gserviceaccount.com --key-file=${GOOGLE_APPLICATION_CREDENTIALS}
 gcloud compute instances stop ${HOSTNAME} --zone ${GCLOUD_ZONE}
